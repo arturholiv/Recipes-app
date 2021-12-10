2aa10f6b6b087569f932572161b0a9862292adea
Alex Menezes <alexmenezes.business@outlook.com>
2021-12-10 14:21:41 -0300

    Feat: adiciona requisição personalizada para a api de Meals

--- src/services/TheMealDbApi.js
+++ src/services/TheMealDbApi.js
@@ -1,16 +1,14 @@
 const RECIPES_ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-const RECIPES_BY_CATEGORY = 'www.themealdb.com/api/json/v1/1/filter.php?c=';
+const RECIPES_BY_CATEGORY = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
 const CATEGORIES_ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
 export async function requestMealDbApi(category) {
   if (category) {
-    console.log('entrei no if');
-    console.log(`${RECIPES_BY_CATEGORY}${category}`);
     const request = await fetch(`${RECIPES_BY_CATEGORY}${category}`);
     const requestJson = await request.json();
     console.log(requestJson);
-    // const { meals } = requestJson;
-    // return meals;
+    const { meals } = requestJson;
+    return meals;
   }
   const request = await fetch(RECIPES_ENDPOINT);
   const requestJson = await request.json();
