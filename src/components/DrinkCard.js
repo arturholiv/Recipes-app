1fe742d8b9b290624d3866b5fd464bf592a18936
Alex Menezes <alexmenezes.business@outlook.com>
2021-12-10 14:18:31 -0300

    Feat: adiciona lógica para redirecionar após clicado

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -1,29 +1,41 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function DrinkCard({ drink: { strDrinkThumb, strDrink }, index }) {
+function DrinkCard({ drink, index }) {
+  const { strDrinkThumb, strDrink, idDrink } = drink;
+  const history = useHistory();
+  const link = `/bebidas/${idDrink}`;
+  console.log(drink);
   return (
-    <div
-      data-testid={ `${index}-recipe-card` }
+    <button
+      type="button"
+      onClick={ () => history.push(link) }
     >
-      <img
-        data-testid={ `${index}-card-img` }
-        src={ strDrinkThumb }
-        alt="img"
-        width="100px"
-      />
-      <h3
-        data-testid={ `${index}-card-name` }
+      <div
+        data-testid={ `${index}-recipe-card` }
       >
-        { strDrink}
+        <img
+          data-testid={ `${index}-card-img` }
+          src={ strDrinkThumb }
+          alt="img"
+          width="100px"
+        />
+        <h3
+          data-testid={ `${index}-card-name` }
+        >
+          { strDrink}
 
-      </h3>
-    </div>
+        </h3>
+      </div>
+
+    </button>
   );
 }
 
 DrinkCard.propTypes = {
   drink: PropTypes.shape({
+    idDrink: PropTypes.string.isRequired,
     strDrink: PropTypes.string,
     strDrinkThumb: PropTypes.string.isRequired,
   }).isRequired,
