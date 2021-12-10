d56055a272b780a5bbf9dc48718019aea345cf33
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-12-10 14:46:22 -0300

    Feat: Header complete

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import Header from '../components/Header';
-
 import Footer from '../components/Footer';
 
 function Comidas() {
