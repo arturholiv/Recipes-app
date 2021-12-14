a31c538bf84932b5defe53920c6546a19379af9d
Antônio Campos <aclcampos@live.com>
2021-12-14 17:53:01 -0300

    Feat: Feito tela explorar por local de origem/area

--- src/pages/ExplorarComidaArea.js
+++ src/pages/ExplorarComidaArea.js
@@ -1,14 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExplorarComidaArea() {
+  const MAX_MEALS_RENDER = 12;
+
+  const [renderNotFound, setRenderNotFound] = useState(false);
+  const [areas, setAreas] = useState([]);
+  const [renderAreas, setRenderAreas] = useState(false);
+  const [meals, setMeals] = useState([]);
+  const [renderMeals, setRenderMeals] = useState(false);
+
+  const history = useHistory();
+
+  const showNotFound = () => {
+    const verifyUSer = localStorage.getItem('user');
+    if (verifyUSer === null) {
+      setRenderNotFound(true);
+    }
+  };
+
+  const getListOfAreas = async () => {
+    const areasList = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+      .then((items) => items.json());
+    setAreas(areasList.meals);
+    setRenderAreas(true);
+  };
+
+  const getMealsByArea = async ({ target }) => {
+    const mealsByArea = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${target.value}`)
+      .then((items) => items.json());
+    setMeals(mealsByArea.meals);
+    setRenderMeals(true);
+  };
+
+  const getMealsByFirstArea = async () => {
+    const mealsByArea = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=American')
+      .then((items) => items.json());
+    setMeals(mealsByArea.meals);
+    setRenderMeals(true);
+  };
+
+  useEffect(() => {
+    showNotFound();
+    getListOfAreas();
+    getMealsByFirstArea();
+  }, []);
+
   return (
     <div>
       <Header />
+      <select data-testid="explore-by-area-dropdown" onChange={ getMealsByArea }>
+        <option data-testid="All-option">All</option>
+        {
+          renderAreas && areas.map(
+            (area) => (
+              <option
+                data-testid={ `${area.strArea}-option` }
+                key={ area.strArea }
+              >
+                {area.strArea}
+              </option>),
+          )
+        }
+      </select>
+      {
+        renderMeals && meals.slice(0, MAX_MEALS_RENDER).map((meal, index) => (
+          <button
+            type="button"
+            key={ meal.strMeal }
+            data-testid={ `${index}-recipe-card` }
+            onClick={ () => history.push(`/comidas/${meal.idMeal}`) }
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ meal.strMealThumb }
+              key={ meal.strMeal }
+              alt={ meal.strMeal }
+              width="100px"
+            />
+            <p
+              key={ meal.strMeal }
+              data-testid={ `${index}-card-name` }
+            >
+              {meal.strMeal}
+
+            </p>
+          </button>
+        ))
+      }
+      {renderNotFound && <h1>Not Found</h1>}
       <Footer />
+
     </div>
   );
 }
 
-export default ExplorarComidaArea;
+export default withRouter(ExplorarComidaArea);
